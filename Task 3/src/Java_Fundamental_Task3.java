bf248194b8e20defd637b2b27a028d87e7cd4330
gvv70 <goyvv70@gmail.com>
2022-08-07 17:40:57 +0300

    The meaning of the expression, task 3;

---
+++ Task 3/src/Java_Fundamental_Task3.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Java_Fundamental_Task3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number x: ");
+        double x = scanner.nextFloat();
+        System.out.print("Enter the number y: ");
+        double y = scanner.nextFloat();
+        double z = (Math.sin(x) + Math.cos(y)) / (Math.cos(x) - Math.sin(y)) * Math.tan(x * y);
+        System.out.println("The value of the z function is " + z);
+    }
+}
