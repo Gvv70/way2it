612853b0d59677ef83df3a0668b8b2a9c7f421a3
gvv70 <goyvv70@gmail.com>
2022-08-07 16:55:09 +0300

    Function value z = ((a - 3) * b / 2) + c;

--- Task 1/src/com/company/Java_Fundamental_Task1.java
+++ Task 1/src/com/company/Java_Fundamental_Task1.java
@@ -2,7 +2,7 @@ package com.company;
 
 import java.util.Scanner;
 
-public class Main {
+public class Java_Fundamental_Task1 {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
